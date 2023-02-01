4621c6211bcc41fc53f232987db1fcb87c4ef76f
2018102248 <jftj97@gmail.com>
2023-02-01 12:29:22 +0900

    Initial commit

---
+++ src/main/java/com/devian/sbootbackend/SbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devian.sbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devian/sbootbackend/SbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devian.sbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
