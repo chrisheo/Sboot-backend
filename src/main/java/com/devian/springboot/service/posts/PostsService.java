47d8585cb84789b84b4eeb95e7effdbdeeb85ff7
2018102248 <jftj97@gmail.com>
2023-02-15 12:47:10 +0900

    [feat] Posts update 관련 로직, 테스트 추가

--- src/main/java/com/devian/springboot/service/posts/PostsService.java
+++ src/main/java/com/devian/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.devian.springboot.service.posts;
 
+import com.devian.springboot.domain.posts.Posts;
 import com.devian.springboot.domain.posts.PostsRepository;
+import com.devian.springboot.web.dto.PostsResponseDto;
 import com.devian.springboot.web.dto.PostsSaveRequestDto;
+import com.devian.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.transaction.annotation.Transactional;
 
 public class PostsService {
@@ -14,5 +17,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
     }
 }
