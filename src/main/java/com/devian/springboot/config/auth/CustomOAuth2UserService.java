04781453a0536f25cc7ccc7f77eee85a14606cfc
2018102248 <jftj97@gmail.com>
2023-02-28 15:58:42 +0900

    [feat] security 부분 skip

--- src/main/java/com/devian/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/devian/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,10 +1,17 @@
 package com.devian.springboot.config.auth;
 
 import com.devian.springboot.domain.user.UserRepository;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import java.util.Collections;
 
 @RequiredArgsConstructor
